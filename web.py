8afa7360cd4d6bd07de39fcf6dc665e746d03254
victorjefferson <vrjefferso33@gmail.com>
2023-01-03 17:50:24 -0800

    update text input

--- web.py
+++ web.py
@@ -25,4 +25,4 @@ for index, todo in enumerate(todos):
         st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new to-do...",
-              on_change=add_todo, key="new_todo")
+              on_change=add_todo, key="new_todo", label_visibility='visible')
